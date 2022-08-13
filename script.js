aa5685fa82b95a359f8fce52ebc7b3bb1ecf0b88
Amit Barman <amitbarrman15fifteen@gmail.com>
2022-08-13 06:19:04 +0530

    add about and contact section

---
+++ script.js
@@ -0,0 +1,24 @@
+
+function removeall(){
+    $('.cir_border').css('border','none');
+}
+$('#sec').on('click',function(){
+    removeall();
+    $('#sec').css('border','2px solid whitesmoke');
+    $('#sec').css('border-radius','20px');
+});
+$('#pri').on('click',function(){
+    removeall();
+    $('#pri').css('border','2px solid whitesmoke');
+    $('#pri').css('border-radius','20px');
+});
+$('#tri').on('click',function(){
+    removeall();
+    $('#tri').css('border','2px solid whitesmoke');
+    $('#tri').css('border-radius','20px');
+});
+$('#quad').on('click',function(){
+    removeall();
+    $('#quad').css('border','2px solid whitesmoke');
+    $('#quad').css('border-radius','20px');
+});
