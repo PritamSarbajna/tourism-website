2803b5051e3fed85f6f52acc64d6f1f8544dc940
krshnknsr21 <krshnknsr21@gmail.com>
2022-09-17 23:04:14 +0530

    UI update for Contact Us section
    Updated the UI for the contact us section and fixed the styling error for the contact navbar button

--- js/script.js
+++ js/script.js
@@ -1,29 +1,33 @@
-
-function removeall(){
-    $('.cir_border').css('border','none');
+function removeall() {
+  $(".cir_border").css("border", "none");
 }
-$('#sec').on('click',function(){
-    removeall();
-    $('#sec').css('border','2px solid whitesmoke');
-    $('#sec').css('border-radius','20px');
+$("#sec").on("click", function () {
+  removeall();
+  $("#sec").css("border", "2px solid whitesmoke");
+  $("#sec").css("border-radius", "20px");
 });
-$('#pri').on('click',function(){
-    removeall();
-    $('#pri').css('border','2px solid whitesmoke');
-    $('#pri').css('border-radius','20px');
+$("#pri").on("click", function () {
+  removeall();
+  $("#pri").css("border", "2px solid whitesmoke");
+  $("#pri").css("border-radius", "20px");
 });
-$('#tri').on('click',function(){
-    removeall();
-    $('#tri').css('border','2px solid whitesmoke');
-    $('#tri').css('border-radius','20px');
+$("#tri").on("click", function () {
+  removeall();
+  $("#tri").css("border", "2px solid whitesmoke");
+  $("#tri").css("border-radius", "20px");
 });
-$('#quad').on('click',function(){
-    removeall();
-    $('#quad').css('border','2px solid whitesmoke');
-    $('#quad').css('border-radius','20px');
+$("#quad").on("click", function () {
+  removeall();
+  $("#quad").css("border", "2px solid whitesmoke");
+  $("#quad").css("border-radius", "20px");
 });
-$('#quint').on('click',function(){
-    removeall();
-    $('#quint').css('border','2px solid whitesmoke');
-    $('#quint').css('border-radius','20px');
+$("#quint").on("click", function () {
+  removeall();
+  $("#quint").css("border", "2px solid whitesmoke");
+  $("#quint").css("border-radius", "20px");
+});
+$("#hex").on("click", function () {
+  removeall();
+  $("#hex").css("border", "2px solid whitesmoke");
+  $("#hex").css("border-radius", "20px");
 });
