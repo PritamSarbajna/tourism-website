53c330d74abd7b2bb18b4a214c0b97b969e0815f
subhranil002 <106914208+subhranil002@users.noreply.github.com>
2023-09-20 23:15:55 +0530

    fixed working of 'Tours' button in nav in index.html, updated navbar ids of index.html, added screen width condition for using removeall() in ./js/script.js, added new element 'hept' in ./js/script.js

--- js/script.js
+++ js/script.js
@@ -1,36 +1,43 @@
-function removeall() {
-    $(".cir_border").css("border", "none");
+if (window.screen.width <= 1130) {
+    function removeall() {
+        $(".cir_border").css("border", "none");
+    }
+    $("#sec").on("click", function () {
+        removeall();
+        $("#sec").css("border", "2px solid whitesmoke");
+        $("#sec").css("border-radius", "20px");
+    });
+    $("#pri").on("click", function () {
+        removeall();
+        $("#pri").css("border", "2px solid whitesmoke");
+        $("#pri").css("border-radius", "20px");
+    });
+    $("#tri").on("click", function () {
+        removeall();
+        $("#tri").css("border", "2px solid whitesmoke");
+        $("#tri").css("border-radius", "20px");
+    });
+    $("#quad").on("click", function () {
+        removeall();
+        $("#quad").css("border", "2px solid whitesmoke");
+        $("#quad").css("border-radius", "20px");
+    });
+    $("#quint").on("click", function () {
+        removeall();
+        $("#quint").css("border", "2px solid whitesmoke");
+        $("#quint").css("border-radius", "20px");
+    });
+    $("#hex").on("click", function () {
+        removeall();
+        $("#hex").css("border", "2px solid whitesmoke");
+        $("#hex").css("border-radius", "20px");
+    });
+    $("#hept").on("click", function () {
+        removeall();
+        $("#hept").css("border", "2px solid whitesmoke");
+        $("#hept").css("border-radius", "20px");
+    });
 }
-$("#sec").on("click", function () {
-    removeall();
-    $("#sec").css("border", "2px solid whitesmoke");
-    $("#sec").css("border-radius", "20px");
-});
-$("#pri").on("click", function () {
-    removeall();
-    $("#pri").css("border", "2px solid whitesmoke");
-    $("#pri").css("border-radius", "20px");
-});
-$("#tri").on("click", function () {
-    removeall();
-    $("#tri").css("border", "2px solid whitesmoke");
-    $("#tri").css("border-radius", "20px");
-});
-$("#quad").on("click", function () {
-    removeall();
-    $("#quad").css("border", "2px solid whitesmoke");
-    $("#quad").css("border-radius", "20px");
-});
-$("#quint").on("click", function () {
-    removeall();
-    $("#quint").css("border", "2px solid whitesmoke");
-    $("#quint").css("border-radius", "20px");
-});
-$("#hex").on("click", function () {
-    removeall();
-    $("#hex").css("border", "2px solid whitesmoke");
-    $("#hex").css("border-radius", "20px");
-});
 
 $("#about").on("mouseover", function () {
     introAboutLogoTransition();
@@ -119,7 +126,6 @@ function updateNav() {
                 navLinks[index].classList.add("active");
             }
         }
-
     });
 }
 
